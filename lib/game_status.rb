2f2bd7217552908e07f9231a970dd45953d0a278
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-25 20:24:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,6 +22,7 @@ def won?(board)
     elsif (board[win_combination[0]] == "O" && board[win_combination[1] == "O"] && board[win_combination[2] == "O"])
       return win_combination
     end
+    false
   end
 end
 
