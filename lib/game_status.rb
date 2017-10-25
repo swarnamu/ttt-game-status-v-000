3c00cb91a878fde194edce1509af848b267d2983
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-25 21:29:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,10 +47,10 @@ def draw?(board)
 end
 
 def over?(board)
-  if draw?(board) == true || won?(board) != nil 
+  if draw?(board) == true || won?(board) != nil
     return true
   else
-    return false  
+    return false
 end
 
 def winner(board)
