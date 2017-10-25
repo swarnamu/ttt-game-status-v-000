66323bde212380896060b57c423318944643b891
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-25 20:48:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,6 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    else
     end
 end
 
