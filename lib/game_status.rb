677fc7a1f80685fe7d879225149abbb1fda90e01
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-25 19:39:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.find do |win_combination|
-    board[win_combination[0]] == board[win_combination[1]] && 
+    board[win_combination[0]] == board[win_combination[1]] &&
     board[win_combination[0]] == board[win_combination[2]] &&
     position_taken(board, win_combination[1])
-    
   end
 end
