b43950cb363e0ea0b92553d61114709ad8a35226
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-26 00:42:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,11 +54,22 @@ def over?(board)
    draw?(board) || full?(board) || won?(board)
 end
 
-def winner(board)
-  if won?(board) != nil
-    winner = board[won?(board)[0]]
-  else
-    draw(board)
-  end
+#def winner(board)
+#  if won?(board) != nil
+#    winner = board[won?(board)[0]]
+#  else
+#    draw(board)
+#  end
 
+#end
+
+def winner(board)
+  winning_board = won?(board)
+  if winning_board == nil
+    return nil
+  elsif board[winning_board[0]].include("X")
+    return "X"
+  else
+    return "O"  
+  end
 end
